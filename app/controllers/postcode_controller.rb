a7a3bb8583aa302e6ad2861e01b6c48e6c3f21bb
isaacjarquin <isaacjarquin@gmail.com>
2021-02-28 21:26:23 +0000

    Move error handling out of the get 'postcode' endpoint

--- app/controllers/postcode_controller.rb
+++ app/controllers/postcode_controller.rb
@@ -6,15 +6,15 @@ require './app/models/postcode/area_checker'
 class InvalidPostcode < StandardError; end
 
 get '/postcode' do
-  begin
-    postcode = UKPostcode.parse(params[:postcode].to_s)
-    raise InvalidPostcode unless postcode.valid?
+  postcode = UKPostcode.parse(params[:postcode].to_s)
+  raise InvalidPostcode unless postcode.valid?
 
-    {
-      code: 200,
-      allowed: Postcode::AreaChecker.allowed?(postcode.to_s.delete(' '))
-    }.to_json
-  rescue InvalidPostcode
-    { code: 400, error: 'Invalid Postcode' }.to_json
-  end
+  {
+    code: 200,
+    allowed: Postcode::AreaChecker.allowed?(postcode.to_s.delete(' '))
+  }.to_json
+end
+
+error InvalidPostcode do
+  { code: 400, error: 'Invalid Postcode' }.to_json
 end
